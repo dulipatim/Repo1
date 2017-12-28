c02aaae5a99ae46b2f00412595f804b7b6524c44
dulipati <manikantadulipati@gmail.com>
2017-12-28 13:09:57 -0800

    modified shell.sh

--- shell.sh
+++ shell.sh
@@ -4,3 +4,15 @@ read fname
 echo "enter lname"
 read lname
 echo "your name is $fname $lname "
+echo "enter your age"
+read age
+diff=`expr 21 - $age`
+#echo  $diff
+if [ $age -ge 21 ]; then
+
+	echo " congrats you are eligible to drive"
+
+else
+	echo "Sorry you are not eligible to drive yet: you have to wait $diff more years to drive"
+
+fi
